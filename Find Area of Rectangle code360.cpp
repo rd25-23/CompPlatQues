6a61e1875a7d4b4b42814b0630a603c49408db40
Ritish Doad <rd0519@srmist.edu.in>
2026-08-03 18:31:58 +0530

    Implement area calculation for rectangle
    This program calculates the area of a rectangle based on user input for length and breadth.
    url-https://www.naukri.com/code360/problems/find-area-of-rectangle_1112574?interviewProblemRedirection=true&attempt_status=COMPLETED&count=25&page=1&search=&sort_entity=order&sort_order=ASC&leftPanelTabValue=PROBLEM&customSource=studio_nav

---
+++ Find Area of Rectangle code360.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int l,b;
+    cin >> l >> b;
+    cout<< l*b;
+
+    return 0;
+}
+/*https://www.naukri.com/code360/problems/find-area-of-rectangle_1112574?interviewProblemRedirection=true&attempt_status=COMPLETED&count=25&page=1&search=&sort_entity=order&sort_order=ASC&leftPanelTabValue=PROBLEM&customSource=studio_nav*/
