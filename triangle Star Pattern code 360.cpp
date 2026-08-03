64e058bc161b66f1d6811680d4b4632c688134c3
Ritish Doad <rd0519@srmist.edu.in>
2026-08-03 16:44:34 +0530

    Add triangle star pattern program in C++
    Implement a program to print a triangle star pattern based on user input.
    url-https://www.naukri.com/code360/problems/triangle-star-pattern_626546?interviewProblemRedirection=true&attempt_status=COMPLETED

---
+++ triangle Star Pattern code 360.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int n;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		for(int j=0; j<=i; j++){
+			cout<<"*";
+		}
+		cout<<"\n";
+	}
+  
+}
+
+
+/*https://www.naukri.com/code360/problems/triangle-star-pattern_626546?interviewProblemRedirection=true&attempt_status=COMPLETED*/
