824e4e710c9a119ed6394587a0ec29ed842f87f3
Ritish Doad <rd0519@srmist.edu.in>
2026-08-03 16:45:52 +0530

    Add comment with problem link in square pattern code
    url-https://www.naukri.com/code360/problems/square-pattern_626545?interviewProblemRedirection=true&attempt_status=COMPLETED&count=25&page=1&search=&sort_entity=order&sort_order=ASC&leftPanelTabValue=PROBLEM&customSource=studio_nav

---
+++ square pattern code360.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/
+	int n;
+	cin >> n;
+	for(int i=0; i<n;i++){
+		for(int j=0; j<n; j++){
+			cout << n;
+		}
+		cout<< "\n";
+	}
+}
+
+/*https://www.naukri.com/code360/problems/square-pattern_626545?interviewProblemRedirection=true&attempt_status=COMPLETED&count=25&page=1&search=&sort_entity=order&sort_order=ASC&leftPanelTabValue=PROBLEM&customSource=studio_nav*/
+
